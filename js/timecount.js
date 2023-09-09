bc939f1ae35edb15be36edeff0c0fd67e2d972b7
XueJie1 <3124792285@qq.com>
2023-09-09 16:49:56 +0800

    Site updated: 2023-09-09 16:49:56

--- js/timecount.js
+++ js/timecount.js
@@ -8,7 +8,7 @@
     var now = Date.now()  /* 当前时间戳 */
     var interval = parseInt(now - pubTime)
     /* 发布时间超过指定时间（毫秒） */
-    if (interval > 3600*24*3){
+    if (interval > 3600*24*60*180){
       var days = parseInt(interval / 86400000)
       posts[0].innerHTML = '<div class="note note-warning" style="font-size:0.9rem"><p>' +
         '<div class="h6">文章时效性提示</div><p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。' +
